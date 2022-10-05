d5577109cb2c2ac2ec21a60e77268c88d2da1541
sourour9 <sourour.gazzeh@outlook.fr>
2022-10-05 08:02:38 +0200

    plya movie

--- server/src/main/java/VODService.java
+++ server/src/main/java/VODService.java
@@ -24,23 +24,24 @@ public class VODService  extends UnicastRemoteObject  implements IVODService  {
      @Override
     public Bill playmovie(String isbn, IClientBox box) {
         List<MovieDesc> movieChoosen=MovieList.stream().filter(movie -> movie.Isbn.equals(isbn)).toList();
-        try{
-            box.stream("Are you ready?".getBytes(StandardCharsets.UTF_8));
-        }catch (RemoteException e){
+         if(!movieChoosen.isEmpty()){
+             try{
+                 box.stream("Are you ready?".getBytes(StandardCharsets.UTF_8));
+             }catch (RemoteException e){
+                 new Bill(movieChoosen.get(0).MovieName,0);
+             }
+             Thread t = new Thread() {
+                 public void run() {
+                     try{
+                         box.stream("Here is the film".getBytes(StandardCharsets.UTF_8));
+                         box.stream("ASSBDIJDSJFOSJFPSKSKepzzpepzpezpzpe".getBytes(StandardCharsets.UTF_8));
 
-        }
-         Thread t = new Thread() {
-             public void run() {
-                 try{
-                     box.stream("Here is the film".getBytes(StandardCharsets.UTF_8));
-                     box.stream("ASSBDIJDSJFOSJFPSKSKepzzpepzpezpzpe".getBytes(StandardCharsets.UTF_8));
-
-                 }catch (RemoteException e){
-
-                 }}};
-         t.start();
-        if(!movieChoosen.isEmpty()) return new Bill(movieChoosen.get(0).MovieName,15);
-
+                     }catch (RemoteException e){
+                         new Bill(movieChoosen.get(0).MovieName,0);
+                     }}};
+             t.start();
+             return new Bill(movieChoosen.get(0).MovieName,15);
+         }
         return null;
 
     }
